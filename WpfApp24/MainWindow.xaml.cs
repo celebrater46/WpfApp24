92df1a098db43bcdf2a9872d9d1f190dc70606b4
celebrater <tkgm45nbchk@gmail.com>
2022-01-11 15:03:35 +0900

    first commit

---
+++ WpfApp24/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp24
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
